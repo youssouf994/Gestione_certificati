ce63c2d685f0f6bf81881fc83dbbf86749962da9
Youssouf A. Toure <toureyoussouf@outlook.it>
2023-03-27 01:14:48 +0200

    -cambiare gestione dati da uno alla volta a solito caricamento di tutti il file in ram -ACCELLERARE

--- Gestione_certificati.cpp
+++ Gestione_certificati.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 #include<string>
+#include<fstream>
+#include<stdlib.h>
+
+
+#define dim 100
+#define dim_data 10
 
 using namespace std;
 
@@ -10,23 +16,231 @@ struct certificato
 		int gg;
 		int mm;
 		int aa;
-	};
+	}data_i;
 
 	struct da_f
 	{
 		int gg;
 		int mm;
 		int aa;
-	};
+	}data_f;
 
+	string nome;
+	string cognome;
 	string mansione;
 	int ore_set;
 };
 
+int quantifica()//dimensione dell'archivio (in voci)
+{
+	int i=0;
+	string app;
 
+	ifstream dime("archivio_dip.txt", ios::in);
+		while(getline(dime, app))
+		{
+			i++;
+		}
+	dime.close();
+
+	return i;
+}
+
+bool check_data(char date[], int d)//controllo data inserita correttamente
+{
+	bool ok=true;
+	int i, x;
+	
+	i = 0;
+
+	while (i != d)
+	{
+		while (i < 2)//controllo giorni
+		{
+			if ((date[i] < 48) || (date[i] > 57))
+			{
+				ok = false;
+			}
+			i++;
+		}
+
+		while (i < 3)//controllo slash
+		{
+			if (date[i] != '/')
+			{
+				ok = false;
+			}
+			i++;
+		}
+
+		while (i < 5)//controllo mesi
+		{
+			if ((date[i] < 48) || (date[i] > 57))
+			{
+				ok = false;
+			}
+			i++;
+		}
+
+		while (i < 6)//controllo slash
+		{
+			if (date[i] != '/')
+			{
+				ok = false;
+			}
+			i++;
+		}
+
+		while (i < 10)//controllo anno
+		{
+			if ((date[i] < 48) || (date[i] > 57))
+			{
+				ok = true;
+			}
+			i++;
+		}
+	}
+
+	return ok;
+}
+
+
+
+void inserimento_ram(certificato certi[], int dime)
+{
+	int i, x, convertito;
+	char app[4];
+	char data[10];
+	bool check = true;
+
+	cout << "nome dipendente" << endl;
+	cin >> certi[dime].nome;
+	cout << "cognome dipendente" << endl;
+	cin >> certi[dime].cognome;
+	cout << "mansione dipendente" << endl;
+	cin >> certi[dime].mansione;
+	cout << "ore settimanali" << endl;
+	cin >> certi[dime].ore_set;
+	cout << "inizio rapporto gg/mm/aaaa" << endl;
+	cin >> data;
+	check = check_data(data, dim_data);
+	if (check == false)
+	{
+		cout << "data errata" << endl;
+	}
+	else
+	{
+		i = 0;
+		while (i < 5)
+		{
+			app[i] = ' ';
+			i++;
+		}
+
+		x = 0;
+		i = 0;
+
+		while (data[i] != '/')
+		{
+			app[x] = data[i];
+			i++;
+			x++;
+		}
+		convertito = atoi(app);
+		certi[dime].data_i.gg = convertito;
+		i++;
+		x = 0;
+
+		while (data[i] != '/')
+		{
+			app[x] = data[i];
+			i++;
+			x++;
+		}
+		convertito = atoi(app);
+		certi[dime].data_i.mm = convertito;
+		i++;
+		x = 0;
+
+		while (data[i] != '/')
+		{
+			app[x] = data[i];
+			i++;
+			x++;
+		}
+		convertito = atoi(app);
+		certi[dime].data_i.aa = convertito;
+		x = 0;
+		check = true;
+	}
+
+
+	cout << "fine rapporto gg/mm/aaaa" << endl;
+	cin >> data;
+	check = check_data(data, dim_data);
+	if (check == false)
+	{
+		cout << "data errata" << endl;
+	}
+	else
+	{
+		i = 0;
+		while (i < 5)
+		{
+			app[i] = ' ';
+			i++;
+		}
+
+		x = 0;
+		i = 0;
+
+		while (data[i] != '/')
+		{
+			app[x] = data[i];
+			i++;
+			x++;
+		}
+		convertito = atoi(app);
+		certi[dime].data_f.gg = convertito;
+		i++;
+		x = 0;
+
+		while (data[i] != '/')
+		{
+			app[x] = data[i];
+			i++;
+			x++;
+		}
+		convertito = atoi(app);
+		certi[dime].data_f.mm = convertito;
+		i++;
+		x = 0;
+
+		while (data[i] != '/')
+		{
+			app[x] = data[i];
+			i++;
+			x++;
+		}
+		convertito = atoi(app);
+		certi[dime].data_f.aa = convertito;
+		x = 0;
+	}
+
+
+}
+
+void salvataggio(certificato certi[], int dime, int posiz)
+{
+	ofstream save("archivio_dip.txt", ios::out, ios::app);
+	save << certi[posiz].nome << "-" << certi[posiz].cognome << "-" << certi[posiz].mansione << "- ore sett. " << certi[posiz].ore_set << "- inizio rapp. " << certi[posiz].data_i.gg << "/" << certi[posiz].data_i.mm << "/" << certi[posiz].data_i.aa << "- fine rapp. " << certi[posiz].data_f.gg << "/" << certi[posiz].data_f.mm << "/" << certi[posiz].data_f.aa << endl;
+	save.close();
+}
 
 int main()
 {
+	certificato certi[dim];
+	int memoria;
 	char scelta;
 
 	do
@@ -43,6 +257,7 @@ int main()
 		{
 			case 'i':
 			case 'I':
+				inserimento_ram(certi, dim);
 			break;
 
 			case 'v':
